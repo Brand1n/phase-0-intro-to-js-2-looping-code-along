1decd7b4d87fbb569e942c44349de1919832dc46
Brandon Tucker <Brandon064@outlook.com>
2023-11-14 13:59:22 -0800

    loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(cards, event){
+    const messages=[]
+    for (let i = 0; i < cards.length; i++){
+        console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!` )
+        messages.push (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+function countDown(count){
+
+while (count >= 0){
+    console.log(count)
+    count--
+}
+}
